32acc10f01b64d14bada2a02c0e05f816135c0ce
ryanfiller <ryanfiller89@gmail.com>
2022-08-08 08:51:49 -0500

    skeleton

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,75 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let hasWindow = typeof window !== 'undefined'
+</script>
+
+<header>
+	make pixel art with css graidents
+</header>
+
+<hr />
+
+<main>
+	{#if hasWindow && !!window.localStorage}
+		// TODO
+	{/if}
+</main>
+
+<footer>
+	<hr />
+	<a href="https://github.com/ryanfiller/pixel-gradients">
+		<strong>code</strong>
+	</a>
+	|
+	<a href="https://github.com/ryanfiller/pixel-gradients/issues">
+		<strong>bugs?</strong>
+	</a>
+	|
+	<a href="https://www.ryanfiller.com?link=pixel-gradients">
+		<strong>ryanfiller.com</strong>
+	</a>
+</footer>
+
+<style>
+	:root {
+		--readable: 50rem;
+	}
+
+	:global(html),
+	:global(body) {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		padding: 0.5rem;
+		height: calc(100vh - 1rem);
+	}
+
+	header {
+		display: grid;
+		gap: 0 1rem;
+		grid-template-columns: auto minmax(auto, var(--readable)) auto;
+	}
+
+	header > * {
+		grid-column: 2 / 3;
+	}
+
+	hr {
+		width: 100%;
+	}
+
+	strong {
+		display: inline;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+	}
+
+	footer {
+		display: flex;
+		gap: 1rem;
+		align-items: center;
+	}
+</style>
