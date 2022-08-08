5e7a861ffd69855feb5ba31f44087a603fc2ec5a
ryanfiller <ryanfiller89@gmail.com>
2022-08-08 09:28:26 -0500

    set up the controls

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,62 @@
 <script>
 	let hasWindow = typeof window !== 'undefined'
+
+	let name = 'pixel-gradient'
+	let rows = 5
+	let columns = 5
+	let foreground = '#00FF00' // lime
+	let background = '#191970' // midnightblue
+
+	$: (
+		hasWindow && (
+			document.documentElement.style.setProperty('--foreground', foreground),
+			document.documentElement.style.setProperty('--background', background)
+		)
+	)
 </script>
 
 <header>
-	make pixel art with css graidents
+	<h1>make pixel art with css graidents!</h1>
 </header>
 
 <hr />
 
 <main>
 	{#if hasWindow && !!window.localStorage}
-		// TODO
+		<fieldset>
+			<label for='name'>
+				<span>name</span>
+				<input type='text' id='name' bind:value={name} />
+			</label>
+			<label for='rows'>
+				<span>rows</span>
+				<input type='number' id='rows' bind:value={rows} />
+			</label>
+			<label for='columns'>
+				<span>columns</span>
+				<input type='number' id='columns' bind:value={columns} />
+			</label>
+			<label for='foreground'>
+				<span>foreground</span>
+				<input type='color' id='foreground' bind:value={foreground} />
+			</label>
+			<label for='background'>
+				<span>background</span>
+				<input type='color' id='background' bind:value={background} />
+			</label>
+		</fieldset>
+
+		<section>
+			<article>
+
+			</article>
+			<article>
+
+			</article>
+			<article>
+
+			</article>
+		</section>
 	{/if}
 </main>
 
@@ -32,6 +78,20 @@
 <style>
 	:root {
 		--readable: 50rem;
+		--foreground: '';
+		--background: ''
+	}
+
+	:global(*) {
+		box-sizing: border-box;
+		color: var(--foreground);
+		border-color: var(--foreground);
+		background-color: var(--background);
+	}
+
+	:global(*):focus {
+		outline: .15em dotted var(--foreground);
+		outline-offset: .125em;
 	}
 
 	:global(html),
@@ -49,10 +109,6 @@
 		grid-template-columns: auto minmax(auto, var(--readable)) auto;
 	}
 
-	header > * {
-		grid-column: 2 / 3;
-	}
-
 	hr {
 		width: 100%;
 	}
@@ -65,6 +121,36 @@
 		flex: 1;
 		display: flex;
 		flex-direction: column;
+	}
+
+	fieldset {
+		display: flex;
+		flex-wrap: wrap;
+		align-items: stretch;
+		gap: 1rem;
+		padding: 1rem;
+		border: none;
+	}
+
+	label {
+		flex: 1;
+	}
+
+	label span {
+		display: block;
+		font-size: 1em;
+	}
+
+	input {
+		width: 100%;
+		min-width: 10em;
+		padding: .5em;
+		border-style: solid;
+	}
+
+	input[type='color'] {
+		padding: 0;
+		height: 2.5em;
 	}
 
 	footer {
